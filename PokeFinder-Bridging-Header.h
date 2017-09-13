ff65dc8c5b6025d746a28fef6555897927c17973
Raivis Olehno <21298759@student.uwl.ac.uk>
2017-09-13 20:51:23 +0100

    GeoFire is working now. Some small updates in ViewController

---
+++ PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  Poke_Finder
+//
+//  Created by Raivis on 13/09/2017.
+//  Copyright © 2017 Raivis. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+#import "GeoFire.h"
+#endif /* PokeFinder_Bridging_Header_h */
